27f67af0a2db1dd4c11a443f91770f3405f06d12
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2024-12-27 18:02:01 +0530

    refactor: added more sample data

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -10,6 +10,28 @@ const sampleData = [
       { action: "Lab Tests", status: "pending", date: "2024-01-01" },
     ],
   },
+  {
+    id: 2,
+    patientName: "Patient 2",
+    contact: "+91 7355015999",
+    parent: "John Doe",
+    immediateActions: [
+      { action: "Vaccines", status: "overdue", date: "2023-01-06" },
+      { action: "Follow-up", status: "pending", date: "2024-12-01" },
+      { action: "Lab Tests", status: "pending", date: "2025-01-05" },
+    ],
+  },
+  {
+    id: 3,
+    patientName: "Patient 3",
+    contact: "+91 7845355670",
+    parent: "Michael",
+    immediateActions: [
+      { action: "Vaccines", status: "overdue", date: "2024-11-01" },
+      { action: "Follow-up", status: "pending", date: "2024-04-19" },
+      { action: "Lab Tests", status: "pending", date: "2023-01-01" },
+    ],
+  },
 ];
 
 export default sampleData;
